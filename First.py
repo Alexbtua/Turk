4aedb0ecc9b63e7e13e2e578ccc84247a4bd6d23
Alexbtua <56979470+Alexbtua@users.noreply.github.com>
2019-11-26 09:50:40 +0300

    according to PEP-8

--- First.py
+++ First.py
@@ -1,22 +1,20 @@
 import zipfile
 import time
-import os
-import shutil
-import sys
-
+# import os
+# import shutil
+# import sys
 
 inputfile = "location.txt"
 outputfile = "log.txt"
 
 f1 = open(inputfile, mode='r', encoding='utf_8')
-f2 = open(outputfile, mode='a', encoding= 'utf_8')
+f2 = open(outputfile, mode='a', encoding='utf_8')
 name_zip = str(time.strftime("%Y%m%d-%H%M%S")) + ".zip"
-zip = zipfile.ZipFile(name_zip, 'w')
+zip_archive = zipfile.ZipFile(name_zip, 'w')
 for num, line in enumerate(f1, 1):
-    zip.write(line.strip(), compress_type=zipfile.ZIP_DEFLATED)
+    zip_archive.write(line.strip(), compress_type=zipfile.ZIP_DEFLATED)
 
-
-zip.close()
+zip_archive.close()
 
 f1.close()
 f2.close()
